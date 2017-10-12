7d198013a8b85392c1b2783b219baa82b4fff34d
Michael Malahe <mmalahe@gmail.com>
2017-10-12 16:31:52 +0200

    Not using this.

--- src/UPGameHandler.py
+++ src/UPGameHandler.py
@@ -2,15 +2,6 @@ from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.action_chains import ActionChains
 
-class FixedDict:
-    def __init__(self, dictionary):
-        self._dictionary = dictionary
-    def __setitem__(self, key, item):
-            if key not in self._dictionary:
-                raise KeyError("The key {} is not defined.".format(key))
-            self._dictionary[key] = item
-    def __getitem__(self, key):
-        return self._dictionary[key]
 
 class UPGameState:
     _scalar_values = {
