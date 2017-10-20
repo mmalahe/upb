90af0f820bafc85c941c06a11c7597c0497c4782
Michael Malahe <Michael Malahe>
2017-10-20 19:42:28 +0200

    Initial emulator class.

--- src/emulation/UPEmulator.py
+++ src/emulation/UPEmulator.py
@@ -1,11 +1,24 @@
 import dukpy
 
-globals_js = open("globals.js", "r")
-
 class UPEmulator:
-    def __init__(self):
-        pass
+    def __init__(self, 
+                 combat_filename="combat.js",
+                 globals_filename="globals.js",
+                 projects_filename="projects.js"):
         
-interpreter = dukpy.JSInterpreter()
-interpreter.evaljs(globals_js.read())
-wireCost = interpreter.evaljs("wireCost")
+        # Set up interpreter
+        self.intp = dukpy.JSInterpreter()
+        
+        # Make initial source read
+        combat_file = open(combat_filename, "r")
+        globals_file = open(globals_filename, "r")
+        projects_file = open(projects_filename, "r")
+        self.intp.evaljs(combat_file.read())
+        self.intp.evaljs(globals_file.read())
+        self.intp.evaljs(projects_file.read())
+        
+    def _getVal(self, name):
+        return self.intp.evaljs(name)
+        
+emu = UPEmulator()
+print(emu._getVal("wireCost"))
