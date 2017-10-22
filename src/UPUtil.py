6af47e2a78e8e05e505910adf9f62b9d7ddbd707
Michael Malahe <Michael Malahe>
2017-10-22 19:13:08 +0200

    Report total reward at end of rollout.

--- src/UPUtil.py
+++ src/UPUtil.py
@@ -1,7 +1,10 @@
 def rollout(env, policy):
     ob = env.reset()
     done = False
+    total_reward = 0.0
     while not done:
         stochastic = True
         ac, vpred = policy.act(stochastic, ob)
         ob, rew, done, _ = env.step(ac)
+        total_reward += rew
+    print("Total reward = {}.".format(total_reward))
