1c6779333d9180ea0b63f3e9182a40f5b0a40604
Michael Malahe <Michael Malahe>
2017-10-22 21:04:51 +0200

    Added check for correct saving.

--- src/policies.py
+++ src/policies.py
@@ -1,6 +1,8 @@
 from baselines.common import tf_util
 from baselines.ppo1.mlp_policy import MlpPolicy
 import pickle
+import tensorflow as tf
+import numpy as np
 
 class MlpPolicySaveable(MlpPolicy):
     def __init__(self, name, *args, **kwargs):
@@ -8,7 +10,7 @@ class MlpPolicySaveable(MlpPolicy):
         
     def save_state(self, filename):
         py_vars = {}
-        for tf_var in self.get_trainable_variables():
+        for tf_var in self.get_variables():
             py_vars[tf_var.name] = tf_var.eval()
         with open(filename, 'wb') as f:
             pickle.dump(py_vars, f)
@@ -17,5 +19,30 @@ class MlpPolicySaveable(MlpPolicy):
         with open(filename, 'rb') as f:
             py_vars = pickle.load(f)
         tf_util.initialize()
-        for tf_var in self.get_trainable_variables():
+        for tf_var in self.get_variables():
             tf_var.load(py_vars[tf_var.name]) 
+            
+    def save_reload_verify(self, filename):
+        """Check that saving and loading restores the same state.
+        """
+        # Initial state of variables
+        py_vars_out = {}
+        for tf_var in self.get_variables():
+            py_vars_out[tf_var.name] = tf_var.eval()
+        
+        # Save
+        self.save_state(filename)
+        
+        # Clear
+        for tf_var in self.get_variables():
+            tf_var = tf.clip_by_value(tf_var, 0.0, 0.0)
+            
+        # Load and compare
+        self.load_state(filename)
+        for tf_var in self.get_variables():
+            py_var_out = py_vars_out[tf_var.name]
+            py_var_in = tf_var.eval()
+            if not np.array_equal(py_var_out, py_var_in):
+                print("{} out =".format(tf_var.name), py_var_out)
+                print("{} in =".format(tf_var.name), py_var_in)
+                raise Exception("Arrays not equal!")         
